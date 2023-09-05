0487dbdc5f8650a0fffd7709d4b045a3be90d97d
Matei87 <mrc_matei@yahoo.com>
2023-09-05 22:55:56 +0300

    fix: initialState, added buton:hover

--- src/context/AppContext.ts
+++ src/context/AppContext.ts
@@ -2,7 +2,6 @@ import { createContext } from 'react';
 import {
   InitialContextMethodsProps,
   InitialContextProps,
-  TaskProp,
 } from '../shared/types';
 import DATA from '../shared/data.json';
 
@@ -12,50 +11,13 @@ export const initialState: InitialContextProps & InitialContextMethodsProps = {
   email: '',
   isUserLoggedIn: false,
   tasks: DATA,
-  setIsUserLoggedIn: (isUserLoggedIn: boolean) => ({
-    ...initialState,
-    isUserLoggedIn,
-  }),
-  setDetails: ({
-    fName,
-    lName,
-    email,
-  }: {
-    fName: string;
-    lName: string;
-    email: string;
-  }) => ({
-    ...initialState,
-    fName,
-    lName,
-    email,
-  }),
-  AddTask: (task: TaskProp) => ({
-    ...initialState,
-    tasks: [...initialState.tasks, task],
-  }),
-  EditTask: (task: TaskProp) => ({
-    ...initialState,
-    tasks: initialState.tasks.map((data) =>
-      data.id === task.id ? task : data
-    ),
-  }),
-  DeleteTask: (id: number) => ({
-    ...initialState,
-    tasks: initialState.tasks.filter((task) => task.id !== id),
-  }),
-  SortDateAsc: () => ({
-    ...initialState,
-    tasks: initialState.tasks.sort(
-      (taskA, taskB) => +new Date(taskA.date) - +new Date(taskB.date)
-    ),
-  }),
-  SortDateDesc: () => ({
-    ...initialState,
-    tasks: initialState.tasks.sort(
-      (taskA, taskB) => +new Date(taskB.date) - +new Date(taskA.date)
-    ),
-  }),
+  setIsUserLoggedIn: () => ({}),
+  setDetails: () => ({}),
+  AddTask: () => ({}),
+  EditTask: () => ({}),
+  DeleteTask: () => ({}),
+  SortDateAsc: () => ({}),
+  SortDateDesc: () => ({}),
 };
 
 export const AppContext = createContext(initialState);
